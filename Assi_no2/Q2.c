181f80dae344101e2dea5602f24d32b625a6c8c8
tejas827 <tejasraje827@gmail.com>
2025-07-15 11:16:08 +0530

    a2

---
+++ Assi_no2/Q2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number:");
+    scanf("%d",&a);
+
+    if(a>=0)
+    printf("positive number");
+    else
+    printf("Negative number");
+} 
