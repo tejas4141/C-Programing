db37e80d670c38fbfd12143e1e1f753dcd34635b
tejas827 <tejasraje827@gmail.com>
2025-07-24 17:11:56 +0530

    per

---
+++ Homework/perfect_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int num,i,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    for(i=1;i<num;i++){
+    if(num %i ==0){
+        sum=sum+i;
+    }
+    }if(sum ==num){
+        printf("%d is a perfect number.\n",num);
+    }else{
+        printf("%d is not perfect number.\n",num);
+    }
+    return 0;
+}
