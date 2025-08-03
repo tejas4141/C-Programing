c690fda2e55c240e31301d3c87da4d3abfc4b5f5
tejas827 <tejasraje827@gmail.com>
2025-08-03 19:04:52 +0530

    Q13

---
+++ Assi_no4/Q13.c
@@ -0,0 +1,14 @@
+//13.	Write a program to print even numbers from 1 to N (or numbers) 
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    if(i%2==0)
+    printf("%d\n",i);
+    }
+    return 0;
+}
