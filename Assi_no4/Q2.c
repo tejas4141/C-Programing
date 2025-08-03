8dfc62eff4b1d6beffee541fa3222d62904cec0e
tejas827 <tejasraje827@gmail.com>
2025-08-03 11:55:44 +0530

    Q2

---
+++ Assi_no4/Q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        printf("Hello\n");
+    }
+    return 0;
+}
