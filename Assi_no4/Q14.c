72273b5ada6d0fa2003d7bf7a69e63431230cff5
tejas827 <tejasraje827@gmail.com>
2025-08-03 19:10:30 +0530

    Q14

---
+++ Assi_no4/Q14.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,i;
+
+    printf("Enter two number:");
+    scanf("%d%d",&x,&y);
+
+    for(i=x;i<=y;i++)
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+    }
+    return 0;
+}
